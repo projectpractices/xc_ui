eb3059c79ea03eecfd59c73e817934e5fb13ec8f
wenhao <921790476@qq.com>
2019-09-15 12:55:44 +0800

    添加查询方法

--- src/views/cms/api/cms.js
+++ src/views/cms/api/cms.js
@@ -1,12 +1,19 @@
 import http from './../../../base/api/public'
-
+import querystring from 'querystring'
 let sysConfig = require('../../../../config/sysConfig');
 let apiUrl = sysConfig.xcApiUrlPre;
 
 //页面查询
 export const page_list = (page, size, params) => {
+  console.log(params);
+  let kv = querystring.stringify(params);
   //请求服务端的页面查询接口
-  return http.requestQuickGet(apiUrl + '/cms/page/list/' + page + '/' + size);
-}
+  return http.requestQuickGet(apiUrl + '/cms/page/pagelist/' + page + '/' + size+'?'+kv);
+};
+
+//查询所有
+export const all_list = (params) => {
+  return http.requestQuickGet(apiUrl + '/cms/page/list/');
+};
 
 
--- src/views/cms/page/PageList.vue
+++ src/views/cms/page/PageList.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
+    <el-select v-model="queryparams.pageId" clearable placeholder="请选择站点">
+      <el-option
+        v-for="item in selectOptionData" :key="item.pageId" :lable="item.pageAliase" :value="item.pageId">
+      </el-option>
+    </el-select>
     <el-button type="primary" plain @click="query()">查询</el-button>
-    <el-table :data="tableData" stripe style="width: 100%">
-      <el-table-column type="index"  width="60"></el-table-column>
+    <el-table :data="tableData" max-height="600" stripe style="width: 100%" >
+      <el-table-column type="index" fixed width="60"></el-table-column>
       <el-table-column prop="pageName"  label="页面名称"  width="120"/>
       <el-table-column prop="pageAliase"  label="别名"  width="120"/>
       <el-table-column prop="pageType"  label="页面类型"  width="150"/>
       <el-table-column prop="pageWebPath"  label="访问路径"  width="250"/>
       <el-table-column prop="pagePhysicalPath"  label="物理路径"  width="250"/>
-      <el-table-column prop="pageCreateTime"  label="创建时间"  width="180"  />
+      <el-table-column prop="pageCreateTime" fixed="right"  label="创建时间"  width="180"  />
     </el-table>
     <el-pagination background layout="prev, pager, next" :total="total" :page-size="params.size"
                    :current-page="params.page"
@@ -23,17 +28,22 @@
         name: 'PageList',
         data() {
             return {
+                selectOptionData: [],
                 tableData: [],
                 total: 0,
                 params: {
                     page: 1,//页码
                     size: 10//每页显示个数
+                },
+                queryparams:{
+                    pageId:'',
+                    pageAliase:''
                 }
             }
         },
         methods: {
             query: function () {
-                cmsApi.page_list(this.params.page, this.params.size).then(res => {
+                cmsApi.page_list(this.params.page, this.params.size,this.queryparams).then(res => {
                     this.tableData = res.queryResult.list;
                     this.total = res.queryResult.total;
                 })
@@ -45,6 +55,10 @@
         },
         mounted() {//钩子方法
             this.query();
+            cmsApi.all_list().then(res=>{
+                this.selectOptionData = res.queryResult.list;
+                console.log(this.selectOptionData)
+            })
         }
     }
 </script>
